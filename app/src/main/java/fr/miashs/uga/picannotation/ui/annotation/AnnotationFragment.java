31160f5274a9811a7af27f0a788afb68fa1b4b2d
Adem Gurbuz <gurbuza@miashs-b1-12.u-ga.fr>
2019-12-13 11:01:22 +0100

    ajustement de l'image

--- app/src/main/java/fr/miashs/uga/picannotation/ui/annotation/AnnotationFragment.java
+++ app/src/main/java/fr/miashs/uga/picannotation/ui/annotation/AnnotationFragment.java
@@ -1,10 +1,15 @@
 package fr.miashs.uga.picannotation.ui.annotation;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +22,9 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import fr.miashs.uga.picannotation.R;
 
@@ -43,7 +51,12 @@ public class AnnotationFragment extends Fragment {
         addContactBtn = (Button)view.findViewById(R.id.buttonContact);
 
         int imageDefault = getResources().getIdentifier("@mipmap/ic_launcher", null, view.getContext().getPackageName());
-        img.setImageResource(imageDefault);
+        //img.setImageResource(imageDefault);
+        Glide.with(this)
+                .load(imageDefault)
+                .apply(new RequestOptions().override(500,500))
+                /*.centerCrop()*/
+                .into(img);
 
         img.setOnClickListener(imgBtnAdd);
         addContactBtn.setOnClickListener(contactAddBtn);
@@ -74,8 +87,32 @@ public class AnnotationFragment extends Fragment {
 
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode == PICK_IMG && resultCode == RESULT_OK){
-            //Log.i("DEBUG", "On a choisi notre image");
-            img.setImageURI(data.getData());
+            Uri imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContext().getContentResolver(), imageUri);
+
+                Display display = this.getActivity().getWindowManager().getDefaultDisplay();
+                Point size = new Point();
+                display.getSize(size);
+                int sw,sh;
+
+                if(bitmap.getHeight() > bitmap.getWidth()) {
+                    sh = size.y / 2;
+                    sw = bitmap.getWidth()*sh/bitmap.getHeight();
+                }
+                else {
+                    sw = size.x;
+                    sh = bitmap.getHeight()*sw/bitmap.getWidth();
+                }
+                Bitmap resized = Bitmap.createScaledBitmap(bitmap, sw, sh, true);
+                Glide.with(this)
+                        .load(resized)
+                        .apply(new RequestOptions().override(sw,sh))
+                        .centerCrop()
+                        .into(img);
+            }
+            catch(Exception e) {
+            }
         }
         if(requestCode == PICK_CONTACT && resultCode == RESULT_OK){
             Log.i("DEBUG", "On a choisi notre contact");
