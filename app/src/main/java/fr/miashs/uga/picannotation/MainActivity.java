d06f1cbad0c2b42c6a1f1a23e3a264565b6cab78
Nicolas <nicolas.gauss.1@orange.fr>
2020-01-11 22:42:00 +0100

    navigationUp

--- app/src/main/java/fr/miashs/uga/picannotation/MainActivity.java
+++ app/src/main/java/fr/miashs/uga/picannotation/MainActivity.java
@@ -1,17 +1,22 @@
 package fr.miashs.uga.picannotation;
 
-import android.app.DatePickerDialog;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,16 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_annotation, R.id.navigation_search)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+
+        navController.navigateUp();
+
+        return super.onSupportNavigateUp();
+    }
 }
